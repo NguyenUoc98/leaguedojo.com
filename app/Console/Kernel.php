bcc904b098e8be4b03ab55a8eae63986f536c8ef
NguyenUoc98 <uocnv.soict.hust@gmail.com>
2020-11-10 01:54:58 +0000

    :smile: update cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('sendmail:birthday')
-                 ->daily('09:01')
+                 ->dailyAt('09:01')
                  ->withoutOverlapping();
     }
 
