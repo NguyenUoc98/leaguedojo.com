f8f05722efdd35f7fc843dd5e56c35e631d239c3
bitran98 <uocnv.soict.hust@gmail.com>
2021-02-01 11:29:54 +0700

    add

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
     <td style="width:150px;">
         <a href="{{ $url }}">
-            <img alt="Logo" height="auto" src="{{ config('app.url') . '/img/core-img/logo2.jpg' }}" style="border:0;display:block;outline:none;text-decoration:none;height:auto;width:100%;font-size:14px;" width="40" />
+            <img alt="Logo" height="auto" src="{{ config('app.url') . '/img/core-img/logo3.jpg' }}" style="border:0;display:block;outline:none;text-decoration:none;height:auto;width:100%;font-size:14px;" width="40" />
             {{ $slot }}
         </a>
     </td>
