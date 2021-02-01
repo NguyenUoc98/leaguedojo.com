a738286edd59480c690b4b6b9c5ddd1d7019271b
bitran98 <uocnv.soict.hust@gmail.com>
2021-02-01 13:28:23 +0700

    update logo header email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
-    <td style="width:150px;">
+    <td style="width:267px;">
         <a href="{{ $url }}">
-            <img alt="Logo" height="auto" src="{{ config('app.url') . '/img/core-img/logo3.jpg' }}" style="border:0;display:block;outline:none;text-decoration:none;height:auto;width:100%;font-size:14px;" width="40" />
+            <img alt="Logo" height="auto" src="{{ config('app.url') . '/img/core-img/logo30.png' }}" style="border:0;display:block;outline:none;text-decoration:none;height:auto;width:100%;font-size:14px;" />
             {{ $slot }}
         </a>
     </td>
