fc667384e92f7d33888a82986c3080209842fc3b
bitran98 <uocnv.soict.hust@gmail.com>
2021-05-30 22:13:17 +0700

    update fb messenger

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -21,12 +21,12 @@
     </div>
     <!-- Load Facebook SDK for JavaScript -->
     <div id="fb-root"></div>
-      <script>
+    <script>
         window.fbAsyncInit = function() {
-          FB.init({
-            xfbml            : true,
-            version          : 'v8.0'
-          });
+            FB.init({
+                xfbml            : true,
+                version          : 'v10.0'
+            });
         };
 
         (function(d, s, id) {
@@ -37,14 +37,11 @@
             fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));</script>
 
-      <!-- Your Chat Plugin code -->
-      <div class="fb-customerchat"
-        attribution=setup_tool
-        page_id="2436630799895751"
-        theme_color="#ed3939"
-        logged_in_greeting="Xin chào! Chúng tôi luôn ở đây sẵn sàng phục vụ bạn."
-        logged_out_greeting="Xin chào! Chúng tôi luôn ở đây sẵn sàng phục vụ bạn.">
-      </div>
+    <!-- Your Chat Plugin code -->
+    <div class="fb-customerchat"
+         attribution="setup_tool"
+         page_id="2436630799895751">
+    </div>
     @include('layouts.footer')
     @include('layouts.script')
 </body>
